e328ad9d0de2a56808343bb03e62c9cee7f57217
B34TER <sew.szymanski@gmail.com>
2023-04-16 18:53:10 +0200

    Add bloc observer

---
+++ lib/app_bloc_observer.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+
+class AppBlocObserver extends BlocObserver {
+  const AppBlocObserver();
+
+  @override
+  void onTransition(
+    Bloc<dynamic, dynamic> bloc,
+    Transition<dynamic, dynamic> transition,
+  ) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    // TODO: implement onEvent
+    print(event);
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    print(error);
+    super.onError(bloc, error, stackTrace);
+  }
+}
