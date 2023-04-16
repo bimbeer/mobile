c51dd82bbf7ff27bce67d028ccbecb5c6d5b34d5
B34TER <sew.szymanski@gmail.com>
2023-04-16 20:01:52 +0200

    Connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:bimbeer/app_bloc_observer.dart';
 import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 import 'core/router/app_router.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = const AppBlocObserver();
   runApp(App(appRouter: AppRouter(),));
 }
