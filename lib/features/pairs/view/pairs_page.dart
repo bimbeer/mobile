f1a67cc7ee6eaab6030e48a0e0de70aa9793254e
B34TER <sew.szymanski@gmail.com>
2023-04-19 16:47:26 +0200

    Create empty pairs page

---
+++ lib/features/pairs/view/pairs_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PairsPage extends StatelessWidget {
+  const PairsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Pairs page'));
+  }
+}
