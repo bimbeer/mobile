1626045b3ed34a4b6b593a4d9549af584f3e123d
B34TER <sew.szymanski@gmail.com>
2023-04-19 16:51:11 +0200

    Update router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,4 +1,6 @@
-import 'package:bimbeer/features/register/view/register_page.dart';
+import 'package:bimbeer/features/authentication/view/login_page.dart';
+import 'package:bimbeer/features/authentication/view/sign_up_page.dart';
+import 'package:bimbeer/features/pairs/view/pairs_page.dart';
 import 'package:flutter/material.dart';
 
 import '../../features/onboard/view/onboard_page.dart';
@@ -9,9 +11,11 @@ class AppRouter {
       case '/':
         return MaterialPageRoute(builder: (_) => const OnboardPage());
       case '/create-account':
-        return MaterialPageRoute(builder: (_) => const RegisterPage());
+        return MaterialPageRoute(builder: (_) => const SignUpPage());
       case '/sign-in':
-        return MaterialPageRoute(builder: (_) => const Placeholder());
+        return MaterialPageRoute(builder: (_) => const LoginPage());
+      case '/pairs':
+        return MaterialPageRoute(builder: (_) => const PairsPage());
       default:
         return null;
     }
