dac09f586cbffaf93aa00616170bc91978129df5
B34TER <sew.szymanski@gmail.com>
2023-04-24 00:11:52 +0200

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bimbeer/app_bloc_observer.dart';
 import 'package:bimbeer/features/authentication/data/repositories/authentication_repository.dart';
+import 'package:bimbeer/features/profile/data/repositories/profile_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -16,7 +17,12 @@ void main() async {
   Bloc.observer = const AppBlocObserver();
 
   final authenticationRepository = AuthenticaionRepository();
-  await authenticationRepository.user.first;
+  final profileRepository = ProfileRepository();
+  final user = await authenticationRepository.user.first;
+  profileRepository.getProfile(user.id).first;
 
-  runApp(App(authenticationRepository: authenticationRepository));
+  runApp(App(
+    authenticationRepository: authenticationRepository,
+    profileRepository: profileRepository,
+  ));
 }
