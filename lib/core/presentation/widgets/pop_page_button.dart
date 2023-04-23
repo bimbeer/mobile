17fb7a74c60707ad7a77c1f5fc9f3670299caab3
B34TER <sew.szymanski@gmail.com>
2023-04-24 00:03:05 +0200

    Rename widget

--- lib/core/presentation/widgets/pop_page_button.dart
+++ lib/core/presentation/widgets/pop_page_button.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 
-class NavigateToOnboardButton extends StatelessWidget {
-  const NavigateToOnboardButton({super.key});
+class PopPageButton extends StatelessWidget {
+  const PopPageButton({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () {
-        Navigator.of(context).popAndPushNamed('/');
+        Navigator.of(context).pop();
       },
       style: ElevatedButton.styleFrom(
         shape: const CircleBorder(),
-        padding: const EdgeInsets.all(5),
         elevation: 0,
         side: const BorderSide(color: Colors.grey),
         backgroundColor: Colors.transparent
