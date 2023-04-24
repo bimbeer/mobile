ec4553ab010b1825f94b735e9ce107121f4bd0d6
B34TER <sew.szymanski@gmail.com>
2023-04-24 18:50:41 +0200

    Modify text theme font

--- lib/core/presentation/theme.dart
+++ lib/core/presentation/theme.dart
@@ -3,12 +3,26 @@ import 'package:google_fonts/google_fonts.dart';
 
 import 'color_schemes.g.dart';
 
+final _font = TextStyle(fontFamily: GoogleFonts.poppins().fontFamily);
+
 final darkTheme = ThemeData.dark().copyWith(
     colorScheme: darkColorScheme,
     textTheme: TextTheme(
-      displaySmall: TextStyle(fontFamily: GoogleFonts.poppins().fontFamily),
-      displayMedium: TextStyle(fontFamily: GoogleFonts.poppins().fontFamily),
-      displayLarge: TextStyle(fontFamily: GoogleFonts.poppins().fontFamily),
+      displaySmall: _font,
+      displayMedium: _font,
+      displayLarge: _font,
+      bodySmall: _font,
+      bodyMedium: _font,
+      bodyLarge: _font,
+      headlineSmall: _font,
+      headlineMedium: _font,
+      headlineLarge: _font,
+      titleSmall: _font,
+      titleMedium: _font,
+      titleLarge: _font,
+      labelSmall: _font,
+      labelMedium: _font,
+      labelLarge: _font,
     )
 );
 
