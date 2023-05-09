28f7ae9c36e61d2c68cdece3e5e5a019c21edad6
B34TER <sew.szymanski@gmail.com>
2023-05-10 00:40:11 +0200

    Remove unnecessary package import

--- lib/features/navigation/view/navigation_bar.dart
+++ lib/features/navigation/view/navigation_bar.dart
@@ -1,4 +1,3 @@
-import 'package:bimbeer/core/presentation/asset_path.dart';
 import 'package:bimbeer/features/navigation/cubit/navigation_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
